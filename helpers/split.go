bdddd63f50a2d2b87b0773b460c0b509091d07ed
Clement <clement.jean@epitech.eu>
2023-05-26 17:29:05 +0800

    Add split and tag examples

---
+++ helpers/split.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+
+	pb "github.com/PacktPublishing/Implementing-gRPC-in-Golang-Microservice/helpers/proto"
+)
+
+func serializedSize[M protoreflect.ProtoMessage](msg M) int {
+	out, err := proto.Marshal(msg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return len(out)
+}
+
+func main() {
+	s := &pb.Split{Name: "Packt"}
+	sz := serializedSize(s)
+
+	fmt.Printf("With Name: %d\n", sz)
+
+	s.Name = ""
+	s.ComplexName = &pb.ComplexName{Name: "Packt"}
+	sz = serializedSize(s)
+
+	fmt.Printf("With ComplexName: %d\n", sz)
+}
---
+++ helpers/tags.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+
+	pb "github.com/PacktPublishing/Implementing-gRPC-in-Golang-Microservice/helpers/proto"
+)
+
+func serializedSize[M protoreflect.ProtoMessage](msg M) int {
+	out, err := proto.Marshal(msg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return len(out)
+}
+
+func main() {
+	t := &pb.Tags{}
+	tags := []int{1, 16, 2048, 262144, 33554432, 536870911}
+	fields := []*int32{&t.Tag, &t.Tag2, &t.Tag3, &t.Tag4, &t.Tag5, &t.Tag6}
+
+	sz := serializedSize(t)
+	fmt.Printf("0 - %d\n", sz)
+
+	for i, f := range fields {
+		*f = 1
+
+		sz := serializedSize(t)
+		fmt.Printf("%d - %d\n", tags[i], sz-(i+1))
+	}
+}
